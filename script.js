4eadbbf12cea7949596cff5d9c2b3312d918cf8f
Erik Vardumyan <100494443+Erik-Vardumyan@users.noreply.github.com>
2023-10-17 23:33:55 -0700

    Incorporated Cart and home section
    Added a Cart to the website that shows image and price. Used event listener so that it only pops up when clicking on cart and disappears when clicking on search bar and vice versa

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const searchIcon = document.querySelector('.search-icon')
 const searchForm = document.querySelector('.search-form')
+
 searchIcon.addEventListener("click", ()=>{
-searchForm.classList.add('active')
+    searchForm.classList.add('active');
+    cartIconContainer.classList.remove('active');
 })
+
+const cartIcon = document.querySelector('.cart-icon')
+const cartIconContainer = document.querySelector('.cart-items-container')
+
+cartIcon.addEventListener("click", ()=>{
+    cartIconContainer.classList.add('active');
+    searchForm.classList.remove('active');
+})
+
