dec003fb4cfa94f78b14c177f5ca2bbaf81f0000
Erik Vardumyan <100494443+Erik-Vardumyan@users.noreply.github.com>
2023-11-07 21:49:39 -0800

    Added responsiveness to different sized resolutions
    The final core step was completing the responsiveness for different resolutions. So when the screen gets smaller the header disappears and can be accessed in the menu icon. and did minor adjustments to the rest of the sections. I have shown the website at its current state to the bakery owner. Discussed minor preference changes and following steps would be to make an active cart system and fill out the Lorems.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const searchIcon = document.querySelector('.search-icon')
-const searchForm = document.querySelector('.search-form')
+const searchIcon = document.querySelector(".search-icon")
+const searchForm = document.querySelector(".search-form")
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
 
 searchIcon.addEventListener("click", ()=>{
-    searchForm.classList.add('active');
-    cartIconContainer.classList.remove('active');
-})
+    searchForm.classList.add("active");
+    cartIconContainer.classList.remove("active");
+    navbar.classList.remove("active");
+});
 
-const cartIcon = document.querySelector('.cart-icon')
-const cartIconContainer = document.querySelector('.cart-items-container')
+menuIcon.addEventListener("click", ()=>{
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    cartIconContainer.classList.remove("active");
+});
+
+const cartIcon = document.querySelector(".cart-icon")
+const cartIconContainer = document.querySelector(".cart-items-container")
 
 cartIcon.addEventListener("click", ()=>{
-    cartIconContainer.classList.add('active');
-    searchForm.classList.remove('active');
-})
+    cartIconContainer.classList.add("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+});
 
+window.onscroll = ()=>{
+    cartIconContainer.classList.remove("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+}
